0b83b2da2a9f22e953fd133f1310d077205b42a3
Imrave <32yoanestebanv@gmail.com>
2025-05-01 10:17:03 -0500

    Se añanadio la base del catalogo de mascotas para implementar la base de datos y probar su funcionalidad

---
+++ js/pest_catalog.js
@@ -0,0 +1,20 @@
+function switchCategory(type) {
+    const cat = document.querySelector('.cat');
+    const dog = document.querySelector('.dog');
+
+    const catImg = document.getElementById('catImg');
+    const dogImg = document.getElementById('dogImg');
+
+    if (type === 'cat') {
+      cat.classList.add('selected');
+      dog.classList.remove('selected');
+      catImg.src = 'media/images/blob_cat.png';
+      dogImg.src = 'media/images/blob_dog_unselect.png';
+    } else {
+      dog.classList.add('selected');
+      cat.classList.remove('selected');
+      catImg.src = 'media/images/blob_cat_unselect.png';
+      dogImg.src = 'media/images/blob_dog.png';
+    }
+  }
+  
